8a0b7f1893084812293b8afe1738dac77f780475
Charles Palmer <charles@Charless-MacBook-Pro.local>
2024-07-31 13:38:38 +0100

    Code style tidy

--- src/Exceptions/ReqresException.php
+++ src/Exceptions/ReqresException.php
@@ -6,7 +6,8 @@ use Exception;
 
 class ReqresException extends Exception
 {
-    public function __toString() {
+    public function __toString()
+    {
         return "Reqres API Failed: [{$this->code}]: {$this->message}\n";
     }
 }
